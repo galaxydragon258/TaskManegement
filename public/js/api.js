c8409698b666e0df08bdff46d21b0e214884c2de
Mark <mjustinee258@gmail.com>
2026-01-12 17:25:10 +0800

    DDD

--- public/js/api.js
+++ public/js/api.js
@@ -1,6 +1,6 @@
 //SERVICEE LAYER DEDICATE TO HANDLE COMMUNICATION SA BACKEND
 
-const API_BASE_URL = 'https://task-manegement-seven.vercel.app/';
+const API_BASE_URL = 'https://task-manegement-syqu.vercel.app/';
 
 //Token Management/
 const getToken = ()=>{
